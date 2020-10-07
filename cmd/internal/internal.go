503252a571e20fc991c09079c540ee838a40c569
flo <flo@cis.lmu.de>
2020-10-07 16:05:14 +0200

    Version v0.0.3

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.2"
+const Version = "v0.0.3"
 
 // IDFromFilePath generates an id based on the file group and the file
 // path.
