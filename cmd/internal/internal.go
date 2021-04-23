445ebf82f28653cf77b275777d3e76c299697937
flo <flo@cis.lmu.de>
2021-04-23 12:48:45 +0200

    Set version to v0.0.31

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.30"
+const Version = "v0.0.31"
 const PStep = "recognition/post-correction"
 
 // IDFromFilePath generates an id based on the file group and the file
