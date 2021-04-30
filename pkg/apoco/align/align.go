a4e50c95f9ee3b9949a622cd127b9e010ee0518c
flo <flo@cis.lmu.de>
2021-04-30 23:21:54 +0200

    Levenshtein-based alignment

--- pkg/apoco/align/align.go
+++ pkg/apoco/align/align.go
@@ -1,7 +1,10 @@
 package align
 
 import (
+	"log"
 	"unicode"
+
+	"git.sr.ht/~flobar/lev"
 )
 
 // Pos represents the start and end position of an alignment.
@@ -62,10 +65,13 @@ func alignAt(spaces []int, str []rune) []Pos {
 	ret := make([]Pos, 0, len(spaces)+1)
 	b := -1
 	for _, s := range spaces {
+		log.Printf("space = %d", s)
 		e := alignmentPos(str, s)
+		log.Printf("e = %d", e)
 		// Var b points to the last found space.
 		// Skip to the next non space token after b.
 		b = skipSpace(str, b+1)
+		log.Printf("e <= b, %d <= %d", e, b)
 		if e <= b { // (e <= b) -> (b>=0) -> len(ret) > 0
 			b = ret[len(ret)-1].B
 		}
@@ -81,6 +87,7 @@ func alignAt(spaces []int, str []rune) []Pos {
 }
 
 func alignmentPos(str []rune, pos int) int {
+	log.Printf("alignmentPos(%s, %d)", string(str), pos)
 	if pos >= len(str) {
 		return len(str)
 	}
@@ -115,4 +122,91 @@ func strip(b, e int, str []rune) (int, int) {
 		e--
 	}
 	return b, e
+}
+
+func Lev(m *lev.Mat, primary []rune, rest ...[]rune) [][]Pos {
+	primary = stripR(primary)
+	var tokens [][]Pos
+	b := -1
+	for i, j := strip(0, len(primary), primary); i < j; i++ {
+		if unicode.IsSpace(primary[i]) {
+			tokens = append(tokens, []Pos{mkpos(b+1, i, primary)})
+			// Skip subsequent whitespace.
+			for i+1 < len(primary) && unicode.IsSpace(primary[i+1]) {
+				i++
+			}
+			b = i
+		}
+	}
+	tokens = append(tokens, []Pos{mkpos(b+1, len(primary), primary)})
+	for _, r := range rest {
+		r = stripR(r)
+		as := alignPair(m, primary, r)
+		if len(as) < len(tokens) {
+			as = append(as, make([]Pos, len(tokens)-len(as))...)
+		}
+		for i := range tokens {
+			tokens[i] = append(tokens[i], as[i])
+		}
+	}
+	return tokens
+}
+
+func alignPair(m *lev.Mat, p, s []rune) []Pos {
+	log.Printf("alignPair(%s, %s)", string(p), string(s))
+	m.DistanceR(p, s)
+	trace := m.TraceR(p, s)
+
+	var pos []Pos
+	var pi, si, pb, sb int
+	log.Printf("trace = %s", trace)
+	for i := range trace {
+		if pi >= len(p) || si >= len(s) {
+			break
+		}
+		if unicode.IsSpace(p[pi]) {
+			end := y(s, si)
+			log.Printf("%d:%d %d:%d", pb, pi, sb, end)
+			// log.Printf("%s %s", string(p[pb:pi]), string(s[sb:end]))
+			pos = append(pos, mkpos(sb, end, s))
+			//pb = z(p, pi)
+			pb = pi + 1
+			if end <= pi {
+				sb = z(s, end)
+			}
+		}
+		switch trace[i] {
+		case '#':
+			pi++
+			si++
+		case '+':
+			si++
+		case '-':
+			pi++
+		}
+	}
+	// end := y(s, si)
+	pos = append(pos, mkpos(sb, len(s), s))
+	log.Printf("%s %s", string(p[pb:pi]), string(s[sb:]))
+	return pos
+}
+
+func y(str []rune, p int) int {
+	for i := p; i < len(str); i++ {
+		if unicode.IsSpace(str[i]) {
+			return i
+		}
+	}
+	return len(str)
+}
+
+func z(str []rune, p int) int {
+	for ; p < len(str) && unicode.IsSpace(str[p]); p++ {
+	}
+	return p
+}
+
+func stripR(str []rune) []rune {
+	b, e := strip(0, len(str), str)
+	return str[b:e]
 }
--- pkg/apoco/align/align_test.go
+++ pkg/apoco/align/align_test.go
@@ -3,6 +3,8 @@ package align
 import (
 	"reflect"
 	"testing"
+
+	"git.sr.ht/~flobar/lev"
 )
 
 func TestDo2(t *testing.T) {
@@ -62,6 +64,39 @@ func TestDo3(t *testing.T) {
 	} {
 		t.Run(tc.master, func(t *testing.T) {
 			pos := Do([]rune(tc.master), []rune(tc.other1), []rune(tc.other2))
+			var got []string
+			for i := range pos {
+				for j := range pos[i] {
+					got = append(got, string(pos[i][j].Slice()))
+				}
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("expected %#v; got %#v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestLev(t *testing.T) {
+	for _, tc := range []struct {
+		master, other string
+		want          []string
+	}{
+		{"", "", []string{"", ""}},
+		{"", "T", []string{"", "T"}},
+		{"", "A B", []string{"", "A B"}},
+		{"T", "", []string{"T", ""}},
+		{"ab cd", "ab cd", []string{"ab", "ab", "cd", "cd"}},
+		{"ab cd", "abcd", []string{"ab", "abcd", "cd", "abcd"}},
+		{"abcd", "ab cd", []string{"abcd", "ab cd"}},
+		{" ab  cd  ", "ab cd", []string{"ab", "ab", "cd", "cd"}},
+		{"n uch ter in", "nuchter in",
+			[]string{"n", "nuchter", "uch", "nuchter", "ter", "nuchter", "in", "in"}},
+		{"a bc  d", "a b d", []string{"a", "a", "bc", "b", "d", "d"}},
+	} {
+		t.Run(tc.master, func(t *testing.T) {
+			var m lev.Mat
+			pos := Lev(&m, []rune(tc.master), []rune(tc.other))
 			var got []string
 			for i := range pos {
 				for j := range pos[i] {
