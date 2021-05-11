f2f97d062b639925fda784e689d9c00898f8d150
flo <flo@cis.lmu.de>
2021-05-11 14:48:18 +0200

    Bump version to v0.0.36

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.35"
+const Version = "v0.0.36"
 const PStep = "recognition/post-correction"
 
 // IDFromFilePath generates an id based on the file group and the file
