597991b80dd90699e453c70acaa30322583bf3af
flo <flo@cis.lmu.de>
2021-06-11 10:58:03 +0200

    Output err dat files

--- cmd/print/dat.go
+++ cmd/print/dat.go
@@ -1,9 +1,7 @@
 package print
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"sort"
 	"strings"
@@ -24,25 +22,31 @@ from stdin, if no FILES.
 }
 
 var datFlags = struct {
-	typ string
+	typ    string
+	limit  int
+	shorts bool
 }{}
 
 func init() {
-	datCMD.Flags().StringVarP(&datFlags.typ, "type", "t", "general", "set type of data")
+	datCMD.Flags().StringVarP(&datFlags.typ, "type", "t", "acc", "set type of data")
+	datCMD.Flags().BoolVarP(&datFlags.shorts, "shorts", "s", false, "consider short errors")
+	datCMD.Flags().IntVarP(&datFlags.limit, "limit", "m", 0, "set candidate limit")
 	CMD.AddCommand(datCMD)
 }
 
 func run(_ *cobra.Command, args []string) {
 	switch datFlags.typ {
-	case "general":
-		runGeneral(args)
+	case "acc":
+		runAcc(args)
+	case "err":
+		runErr(datFlags.limit, datFlags.shorts, args)
 	default:
 		panic("bad type: " + datFlags.typ)
 	}
 }
 
-func runGeneral(files []string) {
-	data := make(map[string][]pair)
+func runAcc(files []string) {
+	data := make(map[string][]accPair)
 	var year, suf string
 	var before, after, total int
 	eachStok(files, func(name string, stok internal.Stok) {
@@ -95,53 +99,87 @@ func runGeneral(files []string) {
 	}
 }
 
+func addpairs(data map[string][]accPair, name, suf string, before, after, total int) {
+	if len(data[name]) == 0 {
+		data[name] = append(data[name], accPair{"OCR", float64(before) / float64(total)})
+	}
+	data[name] = append(data[name], accPair{suf, float64(after) / float64(total)})
+}
+
+type accPair struct {
+	name string
+	data float64
+}
+
+func runErr(limit int, shorts bool, files []string) {
+	var year string
+	data := make(map[string]map[string]int)
+	eachStok(files, func(name string, stok internal.Stok) {
+		if len(name) >= 4 && (year == "" || !strings.HasPrefix(name, year)) {
+			year = name[0:4]
+			data[year] = make(map[string]int)
+		}
+		if stok.Short && !shorts {
+			return
+		}
+		switch stok.Type() {
+		case internal.InfelicitousCorrection:
+			data[year]["infel c"]++
+		case internal.FalseFriend:
+			data[year]["false friends"]++
+		case internal.MissedOpportunity:
+			data[year]["missed op"]++
+		}
+		if stok.ErrAfter() {
+			if stok.Short {
+				data[year]["short errors"]++
+			}
+			switch stok.Cause(limit) {
+			case internal.BadLimit:
+				data[year]["bad limit"]++
+			case internal.MissingCandidate:
+				data[year]["missing c"]++
+			case internal.BadRank:
+				data[year]["bad rank"]++
+			}
+			data[year]["total"]++
+		}
+	})
+	printErrs(shorts, data)
+}
+
+func printErrs(shorts bool, data map[string]map[string]int) {
+	fmt.Printf("#")
+	for k := range data {
+		fmt.Printf(" %s", k)
+	}
+	fmt.Println()
+	names := []string{"short errors", "missing c", "bad limit", "bad rank", "missed op", "infel c"}
+	if !shorts {
+		names = names[1:]
+	}
+	for _, t := range names {
+		fmt.Printf("%q", t)
+		for k := range data {
+			fmt.Printf(" %g", float64(data[k][t])/float64(data[k]["total"]))
+		}
+		fmt.Println()
+	}
+}
+
 func eachStok(files []string, f func(string, internal.Stok)) {
 	if len(files) == 0 {
-		eachStokR(os.Stdin, f)
+		chk(internal.EachStok(os.Stdin, f))
 		return
 	}
 	for _, file := range files {
-		eachStokF(file, f)
+		eachStokInFile(file, f)
 	}
 }
 
-func eachStokF(file string, f func(string, internal.Stok)) {
+func eachStokInFile(file string, f func(string, internal.Stok)) {
 	r, err := os.Open(file)
 	chk(err)
 	defer r.Close()
-	eachStokR(r, f)
-}
-
-func eachStokR(r io.Reader, f func(string, internal.Stok)) {
-	s := bufio.NewScanner(r)
-	var name string
-	for s.Scan() {
-		line := s.Text()
-		if len(line) == 0 {
-			continue
-		}
-		if strings.HasPrefix(line, "#name=") {
-			name = strings.Trim(line[6:], " \t\n")
-			continue
-		}
-		if line[0] == '#' {
-			continue
-		}
-		stok, err := internal.MakeStok(line)
-		chk(err)
-		f(name, stok)
-	}
-	chk(s.Err())
-}
-
-func addpairs(data map[string][]pair, name, suf string, before, after, total int) {
-	if len(data[name]) == 0 {
-		data[name] = append(data[name], pair{"OCR", float64(before) / float64(total)})
-	}
-	data[name] = append(data[name], pair{suf, float64(after) / float64(total)})
-}
-
-type pair struct {
-	name string
-	data float64
+	chk(internal.EachStok(r, f))
 }
