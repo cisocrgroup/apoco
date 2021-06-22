adb61a9016bddf828a13ef30a2138596e9fa15b0
flo <flo@cis.lmu.de>
2021-06-22 13:34:48 +0200

    Bump version v0.0.46-lev

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.46"
+const Version = "v0.0.46-lev"
 const PStep = "recognition/post-correction"
 
 // IDFromFilePath generates an id based on the file group and the file
