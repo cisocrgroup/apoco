aa1b6ecf88c946f0e49e689482e2e3405735290e
flo <flo@cis.lmu.de>
2021-07-12 17:11:09 +0200

    Bump version v0.0.50

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.49"
+const Version = "v0.0.50"
 
 // PStep defines the OCR-D processing step.
 const PStep = "recognition/post-correction"
