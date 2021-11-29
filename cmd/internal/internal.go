68feb1f188a8d2e25cb8d929c990018fbd4f1efd
flobar <flobar@protonmail.com>
2021-11-29 16:51:09 +0100

    Bump version v0.0.60

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.59"
+const Version = "v0.0.60"
 
 // PStep defines the OCR-D processing step.
 const PStep = "recognition/post-correction"
