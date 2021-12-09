36dcc5ca57bb74e75d7962c78c8d316617836191
flobar <f.fink@taxi-fink.de>
2021-12-09 15:20:16 +0100

    Bump version v0.0.62

--- cmd/internal/internal.go
+++ cmd/internal/internal.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version defines the version of apoco.
-const Version = "v0.0.61"
+const Version = "v0.0.62"
 
 // PStep defines the OCR-D processing step.
 const PStep = "recognition/post-correction"
