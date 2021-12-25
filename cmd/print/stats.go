5488910e1f94f0e26d694818a752980d25fd97ed
flobar <f.fink@taxi-fink.de>
2021-12-25 22:04:01 +0100

    Improve output format

--- cmd/print/stats.go
+++ cmd/print/stats.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"text/tabwriter"
 	"unicode/utf8"
 
 	"git.sr.ht/~flobar/apoco/cmd/internal"
@@ -162,75 +163,77 @@ func (s *stats) write(name string, verbose bool) {
 	corbefore, corafter := s.tokenTotal-s.tokenErrBefore, s.tokenTotal-s.tokenErrAfter
 	improvement := s.improvement()
 	charImprovement := ((charErrRateAfter - charErrRateBefore) / charErrRateBefore) * 100
-	fmt.Printf("Name                            = %s\n", name)
-	fmt.Printf("Improvement (chars, percent)    = %g\n", -charImprovement)
-	fmt.Printf("Char error rate (before/after)  = %g/%g\n", charErrRateBefore, charErrRateAfter)
-	fmt.Printf("Char errors (before/after)      = %d/%d\n", s.charErrBefore, s.charErrAfter)
-	fmt.Printf("Total chars                     = %d\n", s.charTotal)
-	fmt.Printf("Susp. error rate (before/after) = %g/%g\n", suspErrRateBefore, suspErrRateAfter)
-	fmt.Printf("Susp. accuracy (before/after)   = %g/%g\n", suspAccBefore, suspAccAfter)
-	fmt.Printf("Suspicious tokens               = %d\n", s.suspTotal)
-	fmt.Printf("Improvement (tokens, percent)   = %g\n", improvement)
-	fmt.Printf("Error rate (before/after)       = %g/%g\n", tokenErrRateBefore, tokenErrRateAfter)
-	fmt.Printf("Accuracy (before/after)         = %g/%g\n", accBefore, accAfter)
-	fmt.Printf("Total errors (before/after)     = %d/%d\n", s.tokenErrBefore, s.tokenErrAfter)
-	fmt.Printf("Correct (before/after)          = %d/%d\n", corbefore, corafter)
-	fmt.Printf("Total tokens                    = %d\n", s.tokenTotal)
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	defer w.Flush()
+	fmt.Fprintf(w, "Name\t%s\n", name)
+	fmt.Fprintf(w, "Improvement (chars, percent)\t%g\n", -charImprovement)
+	fmt.Fprintf(w, "Char error rate (before/after)\t%g/%g\n", charErrRateBefore, charErrRateAfter)
+	fmt.Fprintf(w, "Char errors (before/after)\t%d/%d\n", s.charErrBefore, s.charErrAfter)
+	fmt.Fprintf(w, "Total chars\t%d\n", s.charTotal)
+	fmt.Fprintf(w, "Susp. error rate (before/after)\t%g/%g\n", suspErrRateBefore, suspErrRateAfter)
+	fmt.Fprintf(w, "Susp. accuracy (before/after)\t%g/%g\n", suspAccBefore, suspAccAfter)
+	fmt.Fprintf(w, "Susp. tokens\t%d\n", s.suspTotal)
+	fmt.Fprintf(w, "Improvement (tokens, percent)\t%g\n", improvement)
+	fmt.Fprintf(w, "Error rate (before/after)\t%g/%g\n", tokenErrRateBefore, tokenErrRateAfter)
+	fmt.Fprintf(w, "Accuracy (before/after)\t%g/%g\n", accBefore, accAfter)
+	fmt.Fprintf(w, "Total errors (before/after)\t%d/%d\n", s.tokenErrBefore, s.tokenErrAfter)
+	fmt.Fprintf(w, "Correct (before/after)\t%d/%d\n", corbefore, corafter)
+	fmt.Fprintf(w, "Total tokens\t%d\n", s.tokenTotal)
 	if !verbose {
-		fmt.Printf("Successful corrections          = %d\n", s.types[internal.SuccessfulCorrection])
-		fmt.Printf("Missed opportunities            = %d\n", s.types[internal.MissedOpportunity])
-		fmt.Printf("Infelicitous corrections        = %d\n", s.types[internal.InfelicitousCorrection])
-		fmt.Printf("False friends                   = %d\n", s.types[internal.FalseFriend])
-		fmt.Printf("Short errors                    = %d\n", s.types[internal.SkippedShortErr])
-		fmt.Printf("Merges                          = %d\n", s.skippedMerges+s.merges)
-		fmt.Printf("Splits                          = %d\n", s.skippedSplits+s.splits)
+		fmt.Fprintf(w, "Successful corrections\t%d\n", s.types[internal.SuccessfulCorrection])
+		fmt.Fprintf(w, "Missed opportunities\t%d\n", s.types[internal.MissedOpportunity])
+		fmt.Fprintf(w, "Infelicitous corrections\t%d\n", s.types[internal.InfelicitousCorrection])
+		fmt.Fprintf(w, "False friends\t%d\n", s.types[internal.FalseFriend])
+		fmt.Fprintf(w, "Short errors\t%d\n", s.types[internal.SkippedShortErr])
+		fmt.Fprintf(w, "Merges\t%d\n", s.skippedMerges+s.merges)
+		fmt.Fprintf(w, "Splits\t%d\n", s.skippedSplits+s.splits)
 		return
 	}
 	totalSkippedShort := s.types[internal.SkippedShort] + s.types[internal.SkippedShortErr]
 	totalSkippedLex := s.types[internal.SkippedLex] + s.types[internal.FalseFriend]
 	totalSkippedNoCand := s.types[internal.SkippedNoCand] + s.types[internal.SkippedNoCandErr]
 	totalSkipped := totalSkippedShort + totalSkippedLex + totalSkippedNoCand
-	fmt.Printf("├─ skipped                      = %d\n", totalSkipped)
-	fmt.Printf("│  ├─ short                     = %d\n", totalSkippedShort)
-	fmt.Printf("│  │  └─ errors                 = %d\n", s.types[internal.SkippedShortErr])
-	fmt.Printf("│  ├─ no candidate              = %d\n", totalSkippedNoCand)
-	fmt.Printf("│  │  └─ errors                 = %d\n", s.types[internal.SkippedNoCandErr])
-	fmt.Printf("│  └─ lexicon entries           = %d\n", totalSkippedLex)
-	fmt.Printf("│     └─ false friends          = %d\n", s.types[internal.FalseFriend])
+	fmt.Fprintf(w, "├─ skipped\t%d\n", totalSkipped)
+	fmt.Fprintf(w, "│  ├─ short\t%d\n", totalSkippedShort)
+	fmt.Fprintf(w, "│  │  └─ errors\t%d\n", s.types[internal.SkippedShortErr])
+	fmt.Fprintf(w, "│  ├─ no candidate\t%d\n", totalSkippedNoCand)
+	fmt.Fprintf(w, "│  │  └─ errors\t%d\n", s.types[internal.SkippedNoCandErr])
+	fmt.Fprintf(w, "│  └─ lexicon entries\t%d\n", totalSkippedLex)
+	fmt.Fprintf(w, "│     └─ false friends\t%d\n", s.types[internal.FalseFriend])
 	totalSusp := s.tokenTotal - totalSkipped
 	totalSuspReplCor := s.types[internal.RedundantCorrection] + s.types[internal.InfelicitousCorrection]
 	totalSuspReplNotCor := s.types[internal.SuccessfulCorrection] + s.types[internal.DoNotCareCorrection]
 	totalSuspRepl := totalSuspReplCor + totalSuspReplNotCor
-	fmt.Printf("└─ suspicious                   = %d\n", totalSusp)
-	fmt.Printf("   ├─ replaced                  = %d\n", totalSuspRepl)
-	fmt.Printf("   │  ├─ ocr correct            = %d\n", totalSuspReplCor)
-	fmt.Printf("   │  │  ├─ redundant corr      = %d\n", s.types[internal.RedundantCorrection])
-	fmt.Printf("   │  │  └─ infelicitous corr   = %d\n", s.types[internal.InfelicitousCorrection])
-	fmt.Printf("   │  │     ├─ bad rank         = %d\n", s.causes[internal.InfelicitousCorrection][internal.BadRank])
-	fmt.Printf("   │  │     ├─ bad limit        = %d\n", s.causes[internal.InfelicitousCorrection][internal.BadLimit])
-	fmt.Printf("   │  │     └─ missing corr     = %d\n", s.causes[internal.InfelicitousCorrection][internal.MissingCandidate])
-	fmt.Printf("   │  └─ ocr not correct        = %d\n", totalSuspReplNotCor)
-	fmt.Printf("   │     ├─ successful corr     = %d\n", s.types[internal.SuccessfulCorrection])
-	fmt.Printf("   │     └─ do not care         = %d\n", s.types[internal.DoNotCareCorrection])
-	fmt.Printf("   │        ├─ bad rank         = %d\n", s.causes[internal.DoNotCareCorrection][internal.BadRank])
-	fmt.Printf("   │        ├─ bad limit        = %d\n", s.causes[internal.DoNotCareCorrection][internal.BadLimit])
-	fmt.Printf("   │        └─ missing corr     = %d\n", s.causes[internal.DoNotCareCorrection][internal.MissingCandidate])
+	fmt.Fprintf(w, "└─ suspicious\t%d\n", totalSusp)
+	fmt.Fprintf(w, "   ├─ replaced\t%d\n", totalSuspRepl)
+	fmt.Fprintf(w, "   │  ├─ ocr correct\t%d\n", totalSuspReplCor)
+	fmt.Fprintf(w, "   │  │  ├─ redundant corr\t%d\n", s.types[internal.RedundantCorrection])
+	fmt.Fprintf(w, "   │  │  └─ infelicitous corr\t%d\n", s.types[internal.InfelicitousCorrection])
+	fmt.Fprintf(w, "   │  │     ├─ bad rank\t%d\n", s.causes[internal.InfelicitousCorrection][internal.BadRank])
+	fmt.Fprintf(w, "   │  │     ├─ bad limit\t%d\n", s.causes[internal.InfelicitousCorrection][internal.BadLimit])
+	fmt.Fprintf(w, "   │  │     └─ missing corr\t%d\n", s.causes[internal.InfelicitousCorrection][internal.MissingCandidate])
+	fmt.Fprintf(w, "   │  └─ ocr not correct\t%d\n", totalSuspReplNotCor)
+	fmt.Fprintf(w, "   │     ├─ successful corr\t%d\n", s.types[internal.SuccessfulCorrection])
+	fmt.Fprintf(w, "   │     └─ do not care\t%d\n", s.types[internal.DoNotCareCorrection])
+	fmt.Fprintf(w, "   │        ├─ bad rank\t%d\n", s.causes[internal.DoNotCareCorrection][internal.BadRank])
+	fmt.Fprintf(w, "   │        ├─ bad limit\t%d\n", s.causes[internal.DoNotCareCorrection][internal.BadLimit])
+	fmt.Fprintf(w, "   │        └─ missing corr\t%d\n", s.causes[internal.DoNotCareCorrection][internal.MissingCandidate])
 	totalSuspNotReplCor := s.types[internal.SuspiciousNotReplacedCorrect] + s.types[internal.DodgedBullet]
 	totalSuspNotReplNotCor := s.types[internal.MissedOpportunity] + s.types[internal.SuspiciousNotReplacedNotCorrectErr]
 	totalSuspNotRepl := totalSuspNotReplCor + totalSuspNotReplNotCor
-	fmt.Printf("   └─ not replaced              = %d\n", totalSuspNotRepl)
-	fmt.Printf("      ├─ ocr correct            = %d\n", totalSuspNotReplCor)
-	fmt.Printf("      │  ├─ ocr accept          = %d\n", s.types[internal.SuspiciousNotReplacedCorrect])
-	fmt.Printf("      │  └─ dodged bullets      = %d\n", s.types[internal.DodgedBullet])
-	fmt.Printf("      │     ├─ bad rank         = %d\n", s.causes[internal.DodgedBullet][internal.BadRank])
-	fmt.Printf("      │     ├─ bad limit        = %d\n", s.causes[internal.DodgedBullet][internal.BadLimit])
-	fmt.Printf("      │     └─ missing corr     = %d\n", s.causes[internal.DodgedBullet][internal.MissingCandidate])
-	fmt.Printf("      └─ ocr not correct        = %d\n", totalSuspNotReplNotCor)
-	fmt.Printf("         ├─ missed opportunity  = %d\n", s.types[internal.MissedOpportunity])
-	fmt.Printf("         └─ skipped do not care = %d\n", s.types[internal.SuspiciousNotReplacedNotCorrectErr])
-	fmt.Printf("            ├─ bad rank         = %d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.BadRank])
-	fmt.Printf("            ├─ bad limit        = %d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.BadLimit])
-	fmt.Printf("            └─ missing corr     = %d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.MissingCandidate])
+	fmt.Fprintf(w, "   └─ not replaced\t%d\n", totalSuspNotRepl)
+	fmt.Fprintf(w, "      ├─ ocr correct\t%d\n", totalSuspNotReplCor)
+	fmt.Fprintf(w, "      │  ├─ ocr accept\t%d\n", s.types[internal.SuspiciousNotReplacedCorrect])
+	fmt.Fprintf(w, "      │  └─ dodged bullets\t%d\n", s.types[internal.DodgedBullet])
+	fmt.Fprintf(w, "      │     ├─ bad rank\t%d\n", s.causes[internal.DodgedBullet][internal.BadRank])
+	fmt.Fprintf(w, "      │     ├─ bad limit\t%d\n", s.causes[internal.DodgedBullet][internal.BadLimit])
+	fmt.Fprintf(w, "      │     └─ missing corr\t%d\n", s.causes[internal.DodgedBullet][internal.MissingCandidate])
+	fmt.Fprintf(w, "      └─ ocr not correct\t%d\n", totalSuspNotReplNotCor)
+	fmt.Fprintf(w, "         ├─ missed opportunity\t%d\n", s.types[internal.MissedOpportunity])
+	fmt.Fprintf(w, "         └─ skipped do not care\t%d\n", s.types[internal.SuspiciousNotReplacedNotCorrectErr])
+	fmt.Fprintf(w, "            ├─ bad rank\t%d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.BadRank])
+	fmt.Fprintf(w, "            ├─ bad limit\t%d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.BadLimit])
+	fmt.Fprintf(w, "            └─ missing corr\t%d\n", s.causes[internal.SuspiciousNotReplacedNotCorrectErr][internal.MissingCandidate])
 }
 
 func errorRates(before, after, total int) (float64, float64) {
